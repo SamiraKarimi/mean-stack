6ba1bae01e975a2faa3f1abb5a54fa3c6252a8c5
SamiraKarimi <samira.ka006@gmail.com>
2020-05-08 01:36:41 -0500

    passing-data-from-context

--- pre-mean/CarCat/server.js
+++ pre-mean/CarCat/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require('ejs');
 const app = express();
 app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
@@ -17,6 +18,31 @@ app.get('/car', (req, res) => {
  })
 app.get('/home',(req,res)=>{
     res.render('home');
+})
+app.get('/firstKitty',(req,res)=>{
+    context={
+        favorite_food:'Spagetti',
+        age:3,
+        sleeping_spots:['under the bed','in a sunbeam'] 
+    }
+    res.render('firstKitty',context);
+})
+app.get('/secKitty',(req,res)=>{
+    context={
+        favorite_food:'meatball',
+        age:4,
+        sleeping_spots: ['under the bed','in the leaving room', 'in the couch']
+    
+    }
+    res.render('secKitty',context);
+})
+app.get('/thirdKitty',(req,res)=>{
+    context={
+        favorite_food:'chicken',
+        age:5,
+        sleeping_spots:['under the bed','in the leaving room', 'in the couch']
+    }
+    res.render('thirdKitty',context);
 })
 
 app.listen(8000, () => console.log("listening on port 8000"));
